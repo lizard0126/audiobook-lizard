746ee9685904322642aa9e20bb7ea69e8b2fe44c
lizard0126 <825456471@qq.com>
2025-02-24 17:52:16 +0800

    新建项目

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,38 @@
 import { Context, Schema } from 'koishi'
 
 export const name = 'audiobook-lizard'
+export const inject = ['database'];
 
-export interface Config {}
+export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
+interface Audiobooks {
+  userId: string
+  albumId: number
+  trackId: number
+}
+
+declare module 'koishi' {
+  interface Tables {
+    Audiobooks: Audiobooks;
+  }
+}
+
 export function apply(ctx: Context) {
-  // write your plugin here
+
+
+  ctx.model.extend('Audiobooks', {
+    userId: 'string',
+    albumId: 'unsigned',
+    trackId: 'unsigned',
+  })
+
+
+
+
+
+
+
+
 }
